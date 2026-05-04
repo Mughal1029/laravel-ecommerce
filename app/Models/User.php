4512e7c39f1e7ef255155c701ae1d4b8024eacda
Mughal1029 <sufyan.sufyan1029@gmail.com>
2026-05-04 21:39:44 +0500

    email change

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 
-class User extends Authenticatable implements MustVerifyEmail{
+class User extends Authenticatable{
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
@@ -41,7 +41,6 @@ class User extends Authenticatable implements MustVerifyEmail{
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
     }
